aa61820724db2001093ce3dc23fb49111c5fada0
Akash Padampalle <akashpadampalle780@gmail.com>
2023-04-25 15:29:10 +0530

    configure ejs and express layouts

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
 const PORT  = process.env.PORT || 8000;
 
 const app = express();
 
 app.use(express.json()) // parsing json object
 app.use(express.urlencoded()); // handling encoded request body
+
+
+// setting up ejs
+app.set(expressLayouts);
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 
 
 
