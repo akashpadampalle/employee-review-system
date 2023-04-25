bef1f24fbbcff6ef2de81c4dbeeca657cd668edf
Akash Padampalle <akashpadampalle780@gmail.com>
2023-04-25 17:08:16 +0530

    use path module to define paths

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
+const path = require('path');
 const db = require('./configs/db_connection');
 const PORT  = process.env.PORT || 8000;
 
@@ -12,7 +13,7 @@ app.use(express.urlencoded()); // handling encoded request body
 // setting up ejs
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set('views', path.join(__dirname, 'views'));
 app.set('layout extractStyles', true);
 app.set('layout extractScripts', true);
 
