063106ac9c2c139eb3eb8c17867ab857943b3648
Akash Padampalle <akashpadampalle780@gmail.com>
2023-05-04 09:12:20 +0530

    setup ejs

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+
 const PORT = process.env.PORT || 8000;
 
 
 const app = express();
 
+// ejs setup
+app.use(expressLayouts());
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
+// extract scripts and styles from webpage body
+app.set('extractStyles', true);
+app.set('extractScripts', true);
 
 
 app.listen(PORT, (err) => {
