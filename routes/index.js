672b3d0bedb7dae8b86890e1bc61a7a46ab09467
Akash Padampalle <akashpadampalle780@gmail.com>
2023-05-04 11:54:32 +0530

    add home.ejs

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+//controllers
+const homepageController = require('../controllers/homepage_controllers');
+
+// signin | signup | home routes
+router.get('/', homepageController.renderHomePage);
 
 
 
